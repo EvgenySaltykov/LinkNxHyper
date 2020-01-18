dbaafc2ec8b5dd5c25e03c63f695a6797e8e57b5
EvgenySaltykov <rabota19801@ramler.ru>
2020-01-18 14:18:32 +0300

    add Create assemble ballMIll tool

--- src/main/java/com/intellij/uiDesigner/core/ParamTool.java
+++ src/main/java/com/intellij/uiDesigner/core/ParamTool.java
@@ -2,7 +2,6 @@ package com.intellij.uiDesigner.core;
 
 import nxopen.NXException;
 import nxopen.cam.CAMSetup;
-import nxopen.cam.InheritableDoubleBuilder;
 import nxopen.cam.NCGroupCollection;
 
 import javax.swing.*;
@@ -61,7 +60,18 @@ class ParamTool {
         }
     }
 
-    private double lengthTool = 0.0;
+
+    private double angleTool = 0.0; //угол при вершине у сферической фрезы
+    private final String ANGLE_TOOL = "^11: vtaperAngle\\(";
+    private final Pattern patternAngleTool = Pattern.compile(ANGLE_TOOL);
+
+    private void findAngleTool(String stringIn, Matcher matcher) {
+        if (matcher.find()) {
+            angleTool = Double.parseDouble(stringIn.substring(matcher.end(), (stringIn.length() - 1)));
+        }
+    }
+
+    private double lengthTool = 0.0; //длинна инструмента до хвостовика, или патрона
     private final String LENGTH_TOOL = "^11: vtaperHeight\\(";
     private final Pattern patternLengthTool = Pattern.compile(LENGTH_TOOL);
 
@@ -71,7 +81,18 @@ class ParamTool {
         }
     }
 
-    private double cutLengthTool = 0.0;
+    private double shankLengthTool = 0.0; // длинна хвостовика, если есть
+    private final String SHANK_LENGTH_TOOL = "^11: vtoolTotalLength\\(";
+    private final Pattern patternShankLengthTool = Pattern.compile(SHANK_LENGTH_TOOL);
+
+    private void findShankLengthTool(String stringIn, Matcher matcher) {
+        if (matcher.find()) {
+            double totalLength = Double.parseDouble(stringIn.substring(matcher.end(), (stringIn.length() - 1)));
+            shankLengthTool = totalLength - lengthTool;
+        }
+    }
+
+    private double cutLengthTool = 0.0; //длинна режущей части
     private final String CUT_LENGTH_TOOL = "^11: vcuttingLength\\(";
     private final Pattern patternCutLengthTool = Pattern.compile(CUT_LENGTH_TOOL);
 
@@ -81,7 +102,53 @@ class ParamTool {
         }
     }
 
-    private int numberTool = 0;
+    private boolean isShank = false; // флаг наличия хвостовика
+    private final String SHANK_TOOL = "^11: vtoolShaftType\\(parametric\\)";
+    private final Pattern patternShankTool = Pattern.compile(SHANK_TOOL);
+
+    private void findShankTool(String stringIn, Matcher matcher) {
+        if (matcher.find()) {
+            isShank = true;
+        }
+    }
+
+    private double diamShankTool = 0.0; //диаметр хвостовика
+    private final String DIAM_SHANK_TOOL = "^11: vtoolShaftDiameter\\(";
+    private final Pattern patternDiamShankTool = Pattern.compile(DIAM_SHANK_TOOL);
+
+    private void findDiamShankTool(String stringIn, Matcher matcher) {
+        if (matcher.find()) {
+            diamShankTool = Double.parseDouble(stringIn.substring(matcher.end(), (stringIn.length() - 1)));
+        }
+    }
+
+    private boolean isHolder = false; //флаг наличия патрона
+    private final String FLAG_HOLDER_TOOL = "^11: *oL\\( x\\[/";
+    private final Pattern patternFlagHolderTool = Pattern.compile(FLAG_HOLDER_TOOL);
+
+    private void findFlagHolderTool(String stringIn, Matcher matcher) {
+        if (matcher.find()) {
+            isHolder = true;
+        }
+    }
+
+    private ArrayList<HolderPairVar> itemHolder = new ArrayList<HolderPairVar>();
+    private String itemHolderTool = "^11: *oL\\( x\\[/";
+    private final Pattern patternItemHolderTool = Pattern.compile(itemHolderTool);
+
+    private void findItemHolder(String stringIn, Matcher matcher) {
+        if (matcher.find()) {
+            String subStr;
+            subStr = stringIn.substring(matcher.end());
+
+            double itemX = Double.parseDouble(subStr.substring(0, subStr.indexOf("]")));
+            double itemY = Double.parseDouble(subStr.substring((subStr.indexOf("/") + 1), subStr.lastIndexOf("]")));
+
+            itemHolder.add(new HolderPairVar(itemX, itemY));
+        }
+    }
+
+    private int numberTool = 0; //номер инструмента
     private final String NUMBER_TOOL = ".*cfg\\(\\*WKZNUMMER ";
     private final Pattern patternNumber = Pattern.compile(NUMBER_TOOL);
 
@@ -91,8 +158,7 @@ class ParamTool {
         }
     }
 
-
-    private String nameTool = "Имя_инструмента_не_найдено";
+    private String nameTool = "Имя_инструмента_не_найдено"; //имя инструмента
     private final String PATTERN_NAME = ".*cfg\\(\\*WKZKOMMENTAR ";
     private final Pattern patternName = Pattern.compile(PATTERN_NAME);
     private boolean isNameTool = false;
@@ -125,8 +191,14 @@ class ParamTool {
                 findId(stringIn, patternId.matcher(stringIn));
                 findTypeTool(stringIn, patternTypeTool.matcher(stringIn));
                 findDiamTool(stringIn, patternDiamTool.matcher(stringIn));
+                findAngleTool(stringIn, patternAngleTool.matcher(stringIn));
                 findLengthTool(stringIn, patternLengthTool.matcher(stringIn));
+                findShankLengthTool(stringIn, patternShankLengthTool.matcher(stringIn));
                 findCutLengthTool(stringIn, patternCutLengthTool.matcher(stringIn));
+                findShankTool(stringIn, patternShankTool.matcher(stringIn));
+                findDiamShankTool(stringIn, patternDiamShankTool.matcher(stringIn));
+                findFlagHolderTool(stringIn, patternFlagHolderTool.matcher(stringIn));
+                findItemHolder(stringIn, patternItemHolderTool.matcher(stringIn));
                 findNumber(stringIn, patternNumber.matcher(stringIn));
                 findName(stringIn, patternName.matcher(stringIn));
             }
@@ -222,11 +294,31 @@ class ParamTool {
             toolBuilder.tlHeightBuilder().setValue(lengthTool);
             toolBuilder.tlDiameterBuilder().setValue(diamTool);
             toolBuilder.tlFluteLnBuilder().setValue(cutLengthTool);
+            toolBuilder.tlTaperAngBuilder().setValue(angleTool);
+            if (isShank) {
+                toolBuilder.setUseTaperedShank(true);
+                toolBuilder.taperedShankDiameterBuilder().setValue(diamShankTool);
+                toolBuilder.taperedShankLengthBuilder().setValue(shankLengthTool);
+                toolBuilder.taperedShankTaperLengthBuilder().setValue(0.0);
+            }
 
-            toolBuilder.tlTaperAngBuilder().setValue(3.0);
-            
-//            toolBuilder.tlNumFlutesBuilder().setValue(5);
-            toolBuilder.setDescription("Example ball mill");
+            double lowerDiam;
+            double upperDiam;
+            double length;
+            if (isHolder) {
+                for (int i = 0; i < (itemHolder.size() - 2); i++) {
+                    lowerDiam = itemHolder.get(i).getX() * 2;
+                    upperDiam = itemHolder.get(i + 1).getX() * 2;
+                    length = itemHolder.get(i + 1).getY() - itemHolder.get(i).getY();
+
+                    toolBuilder.holderSectionBuilder().addByUpperDiameter(i, lowerDiam, length, upperDiam, 0.0);
+                }
+            }
+            toolBuilder.tlNumberBuilder().setValue(numberTool);
+            toolBuilder.tlAdjRegBuilder().setValue(numberTool);
+            toolBuilder.tlCutcomRegBuilder().setValue(numberTool);
+            toolBuilder.setDescription("Tool Exported HyperMill")
+            ;
             toolBuilder.commit();
             toolBuilder.destroy();
         } catch (NXException e) {
@@ -237,5 +329,22 @@ class ParamTool {
             e.printStackTrace();
         }
     }
+}
 
+class HolderPairVar {
+    private double x;
+    private double y;
+
+    HolderPairVar(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double getX() {
+        return this.x;
+    }
+
+    double getY() {
+        return this.y;
+    }
 }
