7a4585324c203f51e5d43f87dff27e342ca15243
EvgenySaltykov <rabota19801@rambler.ru>
2020-01-19 19:58:54 +0300

    fixed mSYS name

--- src/main/java/com/intellij/uiDesigner/core/SystemCoordinateBlank.java
+++ src/main/java/com/intellij/uiDesigner/core/SystemCoordinateBlank.java
@@ -9,16 +9,17 @@ import javax.swing.*;
 import java.io.File;
 import java.rmi.RemoteException;
 import java.util.logging.Level;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class SystemCoordinateBlank {
     private File file;
-    private static double[] sysOld = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-    private static final String PERFIX_WKS = "HYPERMILL_WKS_";
-    private static int countSys = 1;
     private int maxReadLine = 2000; //максимальное колличество прочтенных сторк, чтобы не читать весь файл целиком
+    private String mSysName = ""; //имя СКС
+    private static final String MSYS_NAME_PATTERN = "^1: cfg\\(\\*NCS_NAME ";
     private static final String MSYS_PATTERN = ".*frameCs_x\\(|.*frameCs_y\\(|.*frameCs_z\\(|.*frameCs_o\\(|.*frameCs_id\\(";
     private static final String GET_ITEM_MSYS_PATTERN = "(^\\d*: frameCs_x\\(|,|\\))|(^\\d*: frameCs_y\\(|,|\\))|(^\\d*: frameCs_z\\(|,|\\))|(^\\d*: frameCs_o\\(|,|\\))|(^\\d*: frameCs_id\\(|,|\\))";
+    private boolean isEmptyMSYS = true;
 
     SystemCoordinateBlank(File file) {
         this.file = file;
@@ -31,6 +32,7 @@ class SystemCoordinateBlank {
         ReadFile reader = new ReadFile(this.file);
         String in;
         double[] num = new double[13];
+        Matcher matcher;
 
         int i = 0;
         while (reader.ready() && maxReadLine > 0) {
@@ -47,47 +49,38 @@ class SystemCoordinateBlank {
                 }
             }
 
+            matcher = Pattern.compile(MSYS_NAME_PATTERN).matcher(in);
+            if (matcher.find()) {
+                mSysName = in.substring(matcher.end(), (in.length() - 1)).replace(" ", "_").replace(":", "_");
+            }
+
             maxReadLine -= 1;
         }
 
         return num;
     }
 
-    private boolean isEqualsSys(double[] sysOld, double[] sysNew) {
-        double dif;
-
-        for (int i = 0; i < sysOld.length; i++) {
-            dif = sysOld[i] - sysNew[i];
-            if (Math.abs(dif) > 0.000000001) {
-                SystemCoordinateBlank.sysOld = sysNew;
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     private void createSysInNx(double[] sys) {
-        if (!isEqualsSys(sysOld, sys)) { //если матрица СКС изменилась
-            try {
-                nxopen.Session theSession = (nxopen.Session) nxopen.SessionFactory.get("Session");
-                nxopen.Part workPart = theSession.parts().work();
-                nxopen.cam.CAMSetup setup = workPart.camsetup();
+        try {
+            nxopen.Session theSession = (nxopen.Session) nxopen.SessionFactory.get("Session");
+            nxopen.Part workPart = theSession.parts().work();
+            nxopen.cam.CAMSetup setup = workPart.camsetup();
 
-                nxopen.cam.NCGroup geometryRoot = setup.getRoot(CAMSetup.View.GEOMETRY);
-                String parentGeometry = geometryRoot.name();
-                nxopen.cam.CAMObject[] geometryRootMembers = geometryRoot.getMembers();
-                for (nxopen.cam.CAMObject member : geometryRootMembers) {
-                    if (member.name().startsWith(PERFIX_WKS)) {
-                        countSys = getCountSys(member.name());
-                    }
+            nxopen.cam.NCGroup geometryRoot = setup.getRoot(CAMSetup.View.GEOMETRY);
+            String parentGeometry = geometryRoot.name();
+            nxopen.cam.CAMObject[] geometryRootMembers = geometryRoot.getMembers();
+            for (nxopen.cam.CAMObject member : geometryRootMembers) {
+                String s = member.name();
+                if (s.equals(mSysName.toUpperCase())) {
+                    isEmptyMSYS = false;
+                    break;
                 }
+            }
 
+            if (isEmptyMSYS) { // если имя СКС не создано
                 nxopen.cam.NCGroup nCGroup1 = ((nxopen.cam.NCGroup) workPart.camsetup().camgroupCollection().findObject(parentGeometry));
                 nxopen.cam.NCGroup nCGroup2;
-                String nameWKS = PERFIX_WKS.concat(Integer.toString(countSys));
-                nCGroup2 = workPart.camsetup().camgroupCollection().createGeometry(nCGroup1, "mill_planar", "MCS", NCGroupCollection.UseDefaultName.FALSE, nameWKS);
-                countSys++;
+                nCGroup2 = workPart.camsetup().camgroupCollection().createGeometry(nCGroup1, "mill_planar", "MCS", NCGroupCollection.UseDefaultName.FALSE, mSysName);
 
                 nxopen.Point3d origin3 = new nxopen.Point3d(sys[10], sys[11], sys[12]);
                 nxopen.Vector3d xDirection1 = new nxopen.Vector3d(sys[1], sys[2], sys[3]);
@@ -106,25 +99,11 @@ class SystemCoordinateBlank {
                 millOrientGeomBuilder1.setMcs(cartesianCoordinateSystem1);
                 millOrientGeomBuilder1.commit();
                 millOrientGeomBuilder1.destroy();
-
-
-            } catch (NXException e) {
-                new PrintLog(Level.WARNING, "!!!Ошибка создания сессии nxopen.Session!!!", e);
-            } catch (RemoteException e) {
-                new PrintLog(Level.WARNING, "!!!Ошибка создания workPart!!!", e);
             }
+        } catch (NXException e) {
+            new PrintLog(Level.WARNING, "!!!Ошибка создания сессии nxopen.Session!!!", e);
+        } catch (RemoteException e) {
+            new PrintLog(Level.WARNING, "!!!Ошибка создания workPart!!!", e);
         }
-    }
-
-    private int getCountSys(String nameWKS) {
-        int ind = nameWKS.lastIndexOf("_");
-        String num = nameWKS.substring(ind + 1);
-        int count = Integer.parseInt(num);
-
-        if (count > countSys) {
-            return ++count;
-        }
-
-        return countSys;
     }
 }
