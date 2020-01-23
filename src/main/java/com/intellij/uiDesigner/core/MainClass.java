adae21bfeabb14f0721a63aeeb041d21c48dc834
EvgenySaltykov <rabota19801@ramler.ru>
2020-01-23 12:19:35 +0300

    fixed MainForm class and pom.xml

--- src/main/java/com/intellij/uiDesigner/core/MainClass.java
+++ src/main/java/com/intellij/uiDesigner/core/MainClass.java
@@ -3,24 +3,21 @@ package com.intellij.uiDesigner.core;
 import nxopen.BaseSession;
 import nxopen.NXException;
 
-import javax.swing.*;
-
 public class MainClass {
 
-    public static void main(String[] args) throws NXException, java.rmi.RemoteException {
-//        nxopen.Session theSession = (nxopen.Session) nxopen.SessionFactory.get("Session");
-//        lw = theSession.listingWindow();
-//        lw.open();
-//        lw.writeLine("");
-//        lw.writeLine("!!! Hello World !!!");
-//        lw.writeLine("");
-
-        //Создать форму для диалога с пользователем
-        MainForm.createForm();
-        System.out.println("");
-    }
-
-    public static int getUnloadOption() {
-        return BaseSession.LibraryUnloadOption.EXPLICITLY;
-    }
+//    public static void main(String[] args) throws NXException, java.rmi.RemoteException {
+////        nxopen.Session theSession = (nxopen.Session) nxopen.SessionFactory.get("Session");
+////        lw = theSession.listingWindow();
+////        lw.open();
+////        lw.writeLine("");
+////        lw.writeLine("!!! Hello World !!!");
+////        lw.writeLine("");
+//
+//        //Создать форму для диалога с пользователем
+//        new MainForm().createForm();
+//    }
+//
+//    public static int getUnloadOption() {
+//        return BaseSession.LibraryUnloadOption.EXPLICITLY;
+//    }
 }
